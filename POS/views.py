c21ce4cf93b14028da23e6025409ec66d6211770
ceal2013 <carlos.ortiz51@inacapmail.cl>
2026-04-16 01:55:26 -0400

    Agregar decorador custom_login_required a la vista de selección de mesas para asegurar que el usuario esté autenticado antes de acceder.

--- POS/views.py
+++ POS/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from . import services
+from MerquenPOS.decorators import custom_login_required
 
+@custom_login_required
 def seleccion_mesas_view(request):
-    if 'usuario_activo' not in request.session:
-        messages.error(request, "Debe iniciar sesión para ver las mesas.")
-        return redirect('login')
-        
+    """
+    Vista protegida. Solo se ejecuta si el decorador valida la sesión.
+    """
     usuario = request.session['usuario_activo']
     puntos_bd = services.getPuntosVenta()
     
     if not puntos_bd:
         return render(request, 'POS/mesas.html', {'error': 'No hay puntos de venta configurados.'})
 
-    # Empaquetamos todo junto de forma limpia
     puntos_con_mesas = []
     for punto in puntos_bd:
         mesas_del_punto = services.getEstadoMesas(punto['codigo'])
         puntos_con_mesas.append({
             'codigo': punto['codigo'],
             'nombre': punto['nombre'],
-            'mesas': mesas_del_punto # Las mesas van guardadas dentro de su propio punto
+            'mesas': mesas_del_punto 
         })
 
     return render(request, 'POS/mesas.html', {
